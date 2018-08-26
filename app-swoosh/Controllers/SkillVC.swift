5d2bb93bec01d5c2b43404b9a6af88aee110faab
NghiaLQ <luuquangnghia97@gmail.com>
2018-08-26 19:46:29 +0700

    Fix the background-sent-to-back bug

--- app-swoosh/Controllers/SkillVC.swift
+++ app-swoosh/Controllers/SkillVC.swift
@@ -17,6 +17,9 @@ class SkillVC: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func onFinishBtnPressed(_ sender: Any) {
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
